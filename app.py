02f50d6db9b4bb85bdf4bbbb61d2b92ce06064f0
Rizky Darmadi <86047764+rizkydarmadi@users.noreply.github.com>
2021-09-06 08:44:47 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+nilai_awal = 400
+nilai_akhir = 450
+
+selisih = nilai_akhir - nilai_awal
+y2= selisih / nilai_awal
+hasil = y2 * 100
+print(float(hasil))
+  
+  
+i = 0
+eps = [400, 450, 500, 550]
+while i < len(eps):
+    x = eps[i]
+    print(x)
+    i += 1
+    
